f9e43955591c54275c90e4f84d9cad45d2907b38
M <mmuller@itba.edu.ar>
2017-05-21 00:08:02 -0300

    empece a armar lista,etc

---
+++ COMPRESOR+DESCOMPRESOR/Tile.cpp
@@ -0,0 +1,35 @@
+
+#include "Tile.h"
+
+// COMPLETAR FUNCIONES
+
+Tile::Tile()
+{
+	//CONSTRUCTOR
+}
+
+Tile::~Tile()
+{
+	//DESTRUCTOR
+}
+
+
+void Tile::toggleSelection()
+{
+
+}
+
+bool Tile::isSelected()
+{
+
+}
+
+string Tile::getFilename()
+{
+
+}
+
+void Tile::draw (uint x0, uint y0) //funcion que dibuja con allegro UN TILE (una imagen)
+{
+
+}
