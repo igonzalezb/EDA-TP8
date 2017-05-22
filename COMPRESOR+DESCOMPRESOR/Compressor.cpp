bf7d826a50dfce32dd479ca12c76f520dba0722f
M <mmuller@itba.edu.ar>
2017-05-22 18:10:22 -0300

    compressor

--- COMPRESOR+DESCOMPRESOR/Compressor.cpp
+++ COMPRESOR+DESCOMPRESOR/Compressor.cpp
@@ -1,26 +1,41 @@
 
 #include "Compressor.h"
 
+
+void Compressor::decode(char* path)
+{
+	lodepng_encode32_file(&rawImg, &imgWidth, &imgHeight, path);
+}
+
+
+
 void Compressor::quadTree(unsigned char* image, unsigned int x0, unsigned int y0,
 	unsigned int xf, unsigned int yf, FILE* fp)
 {
 	double Rmed = 0.0, Gmed = 0.0, Bmed = 0.0;
-	unsigned int maxR, maxG, maxB, minR, minG, minB;
+	unsigned int Rmax, GmaxG, Bmax, Rmin, Gmin, Bmin;
 
 	unsigned int i;
-	for (i = 0; i < ....; i++) //COMPLETAR
+	for (i = 0; i < ....; i++) //COMPLETAR para determinar i
 	{
-		Rmax = max(Rmax, Img[i]);
-		Rmax = max(Gmax, Img[i+1]);
-		Rmax = max(Bmax, Img[i+2]);
+		Rmax = max(Rmax, Img[i+COMPONENTE_R]);
+		Rmax = max(Gmax, Img[i+COMPONENTE_G]);
+		Rmax = max(Bmax, Img[i+COMPONENTE_B]);
 
-		Rmin = min(Rmin, Img[i]);
-		Rmin = min(Rmin, Img[i+1]);
-		Rmin = min(Rmin, Img[i+2]);
+		Rmin = min(Rmin, Img[i+COMPONENTE_R]);
+		Rmin = min(Rmin, Img[i+COMPONENTE_G]);
+		Rmin = min(Rmin, Img[i+COMPONENTE_B]);
 
-		Rmed += Img[i];
-		Gmed += Img[i+1];
-		Bmed += Img[i+2];
+		Rmed += Img[i+COMPONENTE_R];
+		Gmed += Img[i+COMPONENTE_G];
+		Bmed += Img[i+COMPONENTE_B];
 	}
-	Rmed / i; //ver y seguir
+	Rmed /= i; //ver y seguir
+	Gmed /= (i + 1);
+	Bmed
+}
+
+unsigned int Compressor::max (unsigned int, unsigned int)
+{
+	return 
 }
--- COMPRESOR+DESCOMPRESOR/Compressor.h
+++ COMPRESOR+DESCOMPRESOR/Compressor.h
@@ -2,15 +2,19 @@
 
 #include <boost/filesystem.hpp>
 #include "lodepng.h"
+#include <string>
+
+enum { COMPONENTE_R, COMPONENTE_G, COMPONENTE_B }componentesRGB;
 
 class Compressor
 {
 private:
 	char* rawImg;
-
+	unsigned int imgWidth;
+	unsigned int imgHeight;
 
 public:
-
+	void decode(char* path); //Descompresor tiene que tener encode
 	void quadTree(unsigned char* image, unsigned int x0, unsigned int y0,
 		unsigned int xf, unsigned int yf, FILE* fp);
 
