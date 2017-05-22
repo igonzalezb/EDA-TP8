d1bf7000092b9cea424d924289bbd48c1275d157
M <mmuller@itba.edu.ar>
2017-05-22 19:02:39 -0300

    ..

--- COMPRESOR+DESCOMPRESOR/Compressor.cpp
+++ COMPRESOR+DESCOMPRESOR/Compressor.cpp
@@ -1,12 +1,12 @@
 
 #include "Compressor.h"
+#include "lodepng.h"
 
 
-void Compressor::decode(char* path)
+void Compressor::decode(char* filePath)
 {
-	lodepng_encode32_file(&rawImg, &imgWidth, &imgHeight, path);
+	lodepng_decode32_file(&Img, &imgWidth, &imgHeight, filePath);
 }
-
 
 
 void Compressor::quadTree(unsigned char* image, unsigned int x0, unsigned int y0,
@@ -19,23 +19,56 @@ void Compressor::quadTree(unsigned char* image, unsigned int x0, unsigned int y0
 	for (i = 0; i < ....; i++) //COMPLETAR para determinar i
 	{
 		Rmax = max(Rmax, Img[i+COMPONENTE_R]);
-		Rmax = max(Gmax, Img[i+COMPONENTE_G]);
-		Rmax = max(Bmax, Img[i+COMPONENTE_B]);
+		Gmax = max(Gmax, Img[i+COMPONENTE_G]);
+		Bmax = max(Bmax, Img[i+COMPONENTE_B]);
 
 		Rmin = min(Rmin, Img[i+COMPONENTE_R]);
-		Rmin = min(Rmin, Img[i+COMPONENTE_G]);
-		Rmin = min(Rmin, Img[i+COMPONENTE_B]);
+		Gmin = min(Rmin, Img[i+COMPONENTE_G]);
+		Bmin = min(Rmin, Img[i+COMPONENTE_B]);
 
 		Rmed += Img[i+COMPONENTE_R];
 		Gmed += Img[i+COMPONENTE_G];
 		Bmed += Img[i+COMPONENTE_B];
 	}
-	Rmed /= i; //ver y seguir
-	Gmed /= (i + 1);
-	Bmed
+	Rmed /= (i+COMPONENTE_R); //ver y seguir
+	Gmed /= (i+COMPONENTE_G);
+	Bmed /= (i+COMPONENTE_B);
 }
 
 unsigned int Compressor::max (unsigned int, unsigned int)
 {
 	return 
+}
+
+
+unsigned int Compressor::min(unsigned int, unsigned int)
+{
+	return
+}
+
+
+int max(int* array, int arraySize)
+{
+	int mayor = array[0];
+
+	for (int i = 0; i < arraySize; i++) {
+		if (array[i] > mayor)
+		{
+			mayor = array[i];
+		}
+	}
+	return mayor;
+}
+
+int min(int* array, int arraySize)
+{
+	int menor = array[0];
+
+	for (int i = 0; i < arraySize; i++) {
+		if (array[i] < menor)
+		{
+			menor = array[i];
+		}
+	}
+	return menor;
 }
--- COMPRESOR+DESCOMPRESOR/Compressor.h
+++ COMPRESOR+DESCOMPRESOR/Compressor.h
@@ -9,14 +9,18 @@ enum { COMPONENTE_R, COMPONENTE_G, COMPONENTE_B }componentesRGB;
 class Compressor
 {
 private:
-	char* rawImg;
+	unsigned char* Img;
 	unsigned int imgWidth;
 	unsigned int imgHeight;
+	
 
 public:
-	void decode(char* path); //Descompresor tiene que tener encode
+	//void decode(char* path); //Descompresor tiene que tener encode
+	void decode(char* path);
+
 	void quadTree(unsigned char* image, unsigned int x0, unsigned int y0,
 		unsigned int xf, unsigned int yf, FILE* fp);
-
+	unsigned int max(unsigned int, unsigned int);
+	unsigned int min(unsigned int, unsigned int);
 };
 
